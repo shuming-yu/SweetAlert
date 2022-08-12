5213748950e9e1d8051484db9fc43aa7f0dd3846
Ben <yu.shuming0829@gmail.com>
2022-08-12 15:39:17 +0800

    sweet alert 之 promise 設計 (input-types)

--- all.js
+++ all.js
@@ -1,25 +1,42 @@
+// Swal.fire({
+//   title: "Are you sure?",
+//   text: "You won't be able to revert this!",
+//   icon: "warning",
+//   showCancelButton: true,
+//   confirmButtonColor: "#3085d6",
+//   cancelButtonColor: "#d33",
+//   confirmButtonText: "Yes, delete it!",
+// }).then((result) => {
+
+//   console.log(result);
+
+//   if (result.isConfirmed) {
+//     Swal.fire(
+//       // "Deleted!",
+//       // "Your file has been deleted.",
+//       // "success"
+//       {
+//         title: "Deleted!",
+//         text: "Your file has been deleted.",
+//         icon: "success"
+//       }
+//     );
+//   }
+// });
+
+
+// input-types : https://sweetalert2.github.io/#input-types
+
 Swal.fire({
-  title: "Are you sure?",
-  text: "You won't be able to revert this!",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Yes, delete it!",
+  icon: 'info',
+  title: '請填寫文字內容',
+  input: 'text'
 }).then((result) => {
-
-  console.log(result);
-
-  if (result.isConfirmed) {
-    Swal.fire(
-      // "Deleted!",
-      // "Your file has been deleted.",
-      // "success"
-      {
-        title: "Deleted!",
-        text: "Your file has been deleted.",
-        icon: "success"
-      }
-    );
-  }
-});
+  // console.log(result);
+  // console.log(result.value);
+  Swal.fire({
+    icon: 'success',
+    title: '新增內容成功',
+    text: result.value
+  })
+})
